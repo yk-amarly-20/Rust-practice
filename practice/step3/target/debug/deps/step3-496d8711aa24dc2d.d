a47038ea7d98d51dd2e2861699181603917b7cd5
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-01 21:33:52 +0900

    add

---
+++ practice/step3/target/debug/deps/step3-496d8711aa24dc2d.d
@@ -0,0 +1,5 @@
+/Users/kojimayuto/Desktop/work_dir/Rust-practice/practice/step3/target/debug/deps/step3-496d8711aa24dc2d: src/main.rs
+
+/Users/kojimayuto/Desktop/work_dir/Rust-practice/practice/step3/target/debug/deps/step3-496d8711aa24dc2d.d: src/main.rs
+
+src/main.rs:
---
+++ practice/step3/target/debug/step3.d
@@ -0,0 +1,1 @@
+/Users/kojimayuto/Desktop/work_dir/Rust-practice/practice/step3/target/debug/step3: /Users/kojimayuto/Desktop/work_dir/Rust-practice/practice/step3/src/main.rs
