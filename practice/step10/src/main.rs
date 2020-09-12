69ce4ba4e3eb9892d1afca378b8384cc8cc2738f
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-12 13:50:02 +0900

    add

---
+++ practice/step10/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    // println!("Hello, world!");
+
+    let mut important_data = "Hello World!".to_string();
+
+    important_data = calc_data(important_data);
+
+    println!("{}", important_data);
+}
+
+fn calc_data(data: String) -> String {
+  println!("{}", data);
+  data
+}
