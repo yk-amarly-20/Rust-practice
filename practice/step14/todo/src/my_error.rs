f39ec3f75b00861bcac6aa61271621e7ec1707a1
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-15 22:02:08 +0900

    fix: エラー対応追加

--- practice/step14/todo/src/my_error.rs
+++ practice/step14/todo/src/my_error.rs
@@ -2,11 +2,19 @@
 use thiserror::Error;
 use actix_web::ResponseError;
 use askama;
+use r2d2;
+use rusqlite;
 
 #[derive(Error, Debug)]
 pub enum MyError {
     #[error("Failed to render HTML")]
-    AskamaError(#[from] askama::Error)
+    AskamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConnectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
 }
 
 impl ResponseError for MyError {}
