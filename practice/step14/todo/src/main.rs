1c83e88701cba09eb6867fee4daf2eb455b74c50
yk-amarly-20 <y.k.83600260@icloud.com>
2020-09-15 19:16:10 +0900

    fix: ダミーデータを用意して

--- practice/step14/todo/src/main.rs
+++ practice/step14/todo/src/main.rs
@@ -1,10 +1,28 @@
 use actix_web::{get, App, HttpResponse, HttpServer};
+extern crate todo;
+use todo::my_error;
+use todo::entry;
+use todo::render_temp;
+use askama::Template;
 
 #[get("/")]
-async fn index() -> Result<HttpResponse, actix_web::Error> {
-    let response_body = "Hello World!";
+async fn index() -> Result<HttpResponse, my_error::MyError> {
+    let mut entries = Vec::new();
+    entries.push(entry::TodoEntry {
+        id: 1,
+        text: "First Entry".to_string(),
+    });
+    entries.push(entry::TodoEntry {
+        id: 2,
+        text: "Second Entry".to_string(),
+    });
 
-    Ok(HttpResponse::Ok().body(response_body))
+    let html = render_temp::IndexTemplate {entries};
+    let response_body = html.render()?;
+
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
